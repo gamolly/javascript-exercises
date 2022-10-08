98f4a19a114d7e4374f3281024c527cbcd9a61cf
Gamolly <gamollygamolly@gmail.com>
2022-10-08 15:38:15 -0400

    Add my solution to file
    I recently became aware of the topic of "time complexity", so I avoided
    using loops. I wrote equations that passe the tests more and that should
    be more efficient than looping.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(nStart, nEnd) {
+
+    nStartType = typeof nStart;
+    nEndType = typeof nEnd;
+
+    if (nStart <0 || nEnd < 0 || nStartType !='number' || nEndType != 'number') {
+        return 'ERROR';
+    }
+    else if (nStart > nEnd){
+        return Math.abs(0.5 * (nEnd ** 2 + nEnd - nStart ** 2 - nStart)-1);
+    }
+    return Math.abs(0.5 * (nEnd ** 2 + nEnd - nStart ** 2 + nStart));
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
